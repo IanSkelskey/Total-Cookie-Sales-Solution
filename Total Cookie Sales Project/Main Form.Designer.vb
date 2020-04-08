9eac71bb7e79ad46e186e7f642269369c039c6e0
Ian Skelskey <46094112+IanSkelskey@users.noreply.github.com>
2020-04-08 10:38:28 -0700

    Add files via upload

---
+++ Total Cookie Sales Project/Main Form.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblPecan = New System.Windows.Forms.Label()
+        Me.lblPeanut = New System.Windows.Forms.Label()
+        Me.lblChocolate = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(-2, -2)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(320, 172)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(213, 190)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 0
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(6, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Chocolate Chip:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblPecan)
+        Me.GroupBox1.Controls.Add(Me.lblPeanut)
+        Me.GroupBox1.Controls.Add(Me.lblChocolate)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 180)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(162, 116)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Totals"
+        '
+        'lblPecan
+        '
+        Me.lblPecan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPecan.Location = New System.Drawing.Point(108, 78)
+        Me.lblPecan.Name = "lblPecan"
+        Me.lblPecan.Size = New System.Drawing.Size(39, 24)
+        Me.lblPecan.TabIndex = 5
+        Me.lblPecan.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblPeanut
+        '
+        Me.lblPeanut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPeanut.Location = New System.Drawing.Point(108, 44)
+        Me.lblPeanut.Name = "lblPeanut"
+        Me.lblPeanut.Size = New System.Drawing.Size(39, 24)
+        Me.lblPeanut.TabIndex = 4
+        Me.lblPeanut.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblChocolate
+        '
+        Me.lblChocolate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblChocolate.Location = New System.Drawing.Point(108, 14)
+        Me.lblChocolate.Name = "lblChocolate"
+        Me.lblChocolate.Size = New System.Drawing.Size(39, 24)
+        Me.lblChocolate.TabIndex = 3
+        Me.lblChocolate.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(6, 83)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(96, 24)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Pecan Sandies:"
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(6, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Peanut Butter:"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(213, 219)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(317, 313)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Cookie Sales"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lblPecan As Label
+    Friend WithEvents lblPeanut As Label
+    Friend WithEvents lblChocolate As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Total Cookie Sales Project/Main Form.vb
@@ -0,0 +1,13 @@
+﻿' Name:         Total Cookie Sales Project
+' Purpose:      Display the total number of boxes of each cookie type sold.
+' Programmer:   <your name> on <current date>
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
